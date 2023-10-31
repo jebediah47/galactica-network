bdcf50155826e0a112a39e8ad43d862e5e73d35d
Christian Llupo <jebediah47@protonmail.com>
2023-10-31 11:10:06 +0200

    Update main.py
    Implemented use of initial balance function

--- src/main.py
+++ src/main.py
@@ -4,7 +4,11 @@ import json
 
 if __name__ == "__main__":
     Chain()
-    alice = Wallet()
-    bob = Wallet()
-    alice.create_transaction(10, bob.wallet_address)
+    alice = Wallet(initial_balance=69.69)
+    bob = Wallet(initial_balance=72.8)
+    print("Initial Balance:", alice.get_balance())
+    print("Initial Balance:", bob.get_balance())
+    alice.create_transaction(72, bob.wallet_address)
+    bob.create_transaction(6.9, alice.wallet_address)
     print(json.dumps(Chain.instance.chain[1].__dict__, indent=4))
+    print(json.dumps(Chain.instance.chain[2].__dict__, indent=4))
