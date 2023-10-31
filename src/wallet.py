669e087042072358df9797350b595fc445d0c4b9
Christian Llupo <jebediah47@protonmail.com>
2023-10-31 11:09:28 +0200

    Update wallet.py
    Added initial balance when creating wallet address

--- src/wallet.py
+++ src/wallet.py
@@ -21,7 +21,18 @@ class Wallet:
     private_key = None
     wallet_address = None
 
-    def __init__(self):
+    # Initialize a dictionary to store user balances
+    balances = {}
+
+    def get_balance(self):
+            # Get the balance for the current wallet address
+            return Wallet.balances.get(self.wallet_address, 0)
+
+    def set_balance(self, initial_balance: float):
+        # Set the initial balance for the wallet address
+        Wallet.balances[self.wallet_address] = initial_balance
+
+    def __init__(self, initial_balance: float = 0.0):
         keypair = rsa.generate_private_key(
             public_exponent=65537,
             key_size=2048
@@ -42,18 +53,26 @@ class Wallet:
             encryption_algorithm=serialization.NoEncryption()
         )
         self.private_key = private_key_bytes.decode("utf-8")
+        self.set_balance(initial_balance)
+    
 
-    def create_transaction(self, amount, recipient):
-        private_key = serialization.load_pem_private_key(
+    def create_transaction(self, amount: float, recipient: str):
+        # Check if the sender has enough balance for the transaction
+        if self.get_balance() >= amount:
+            Wallet.balances[self.wallet_address] -= amount
+            private_key = serialization.load_pem_private_key(
             self.private_key.encode("utf-8"),
             password=None
-        )
-        transaction = Transaction(amount, self.wallet_address, recipient)
-        sig = private_key.sign(
+            )
+            transaction = Transaction(amount, self.wallet_address, recipient)
+            sig = private_key.sign(
             hashlib.sha256(transaction.to_json().encode("ascii")).hexdigest().encode(),
-            padding.PSS(
-                mgf=padding.MGF1(hashes.SHA256()),
-                salt_length=padding.PSS.MAX_LENGTH),
-            hashes.SHA256()
-        )
-        Chain.instance.add_block(transaction=transaction, sender_public_key=self.public_key, signature=sig)
+                padding.PSS(
+                    mgf=padding.MGF1(hashes.SHA256()),
+                    salt_length=padding.PSS.MAX_LENGTH),
+                hashes.SHA256()
+            )
+            Chain.instance.add_block(transaction=transaction, sender_public_key=self.public_key, signature=sig)
+            print(self.balances)
+        else:
+            print("Insufficient balance for the transaction.")
